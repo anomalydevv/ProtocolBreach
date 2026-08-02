06ee4811a78fd20661179520bff54cbbafeb653d
anomalydevv <anomalydevv@gmail.com>
2026-08-02 22:05:43 +0300

    ProjectClean

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,12 +8,14 @@ version = "1.0-SNAPSHOT"
 val gdxVersion = "1.12.1"
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
     implementation("com.badlogicgames.gdx:gdx:$gdxVersion")
     implementation("com.badlogicgames.gdx:gdx-backend-lwjgl3:$gdxVersion")
     implementation("com.badlogicgames.gdx:gdx-platform:$gdxVersion:natives-desktop")
+    implementation("com.github.mgsx-dev.gdx-gltf:gltf:2.3.0")
 
     testImplementation(kotlin("test"))
 }
--- src/main/kotlin/Main.kt
+++
@@ -1,8 +0,0 @@
-package org.anomalydevv
-
-import org.anomalydevv.core.DesktopLauncher
-
-fun main(args: Array<String>) {
-    DesktopLauncher.launc()
-
-}
